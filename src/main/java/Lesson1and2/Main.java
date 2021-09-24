a2e4fdc01720b638665139fa826a95fc514a67e5
Crfkf1984! <seirosso1@gmail.com>
2021-09-25 00:20:10 +0300

    Lesson 1.2

---
+++ src/main/java/Lesson1and2/Main.java
@@ -0,0 +1,11 @@
+package Lesson1and2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("********************************");
+        System.out.println("***** " + "      Визитка       " + " *****");
+        System.out.println("***** " + " Name " + " : you name " + "   *****");
+        System.out.println("***** " + " Skype " + " : you skype " + " *****");
+        System.out.println("********************************");
+    }
+}
