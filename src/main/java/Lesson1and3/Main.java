e10cf586b006b99a284a26702b57f9888665cd18
Crfkf1984! <seirosso1@gmail.com>
2021-09-25 12:03:08 +0300

    Lesson 1.1

---
+++ src/main/java/Lesson1and3/Main.java
@@ -0,0 +1,11 @@
+package Lesson1and3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("********************************" + "\n");
+        System.out.print("***** " + "      Визитка       " + " *****" + "\n");
+        System.out.print("***** " + " Name " + " : you name " + "   *****" + "\n");
+        System.out.print("***** " + " Skype " + " : you skype " + " *****" + "\n");
+        System.out.print("********************************");
+    }
+}
