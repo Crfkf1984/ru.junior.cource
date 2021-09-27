4080aa49f8ec06e6c0740912130444590194ae82
Crfkf1984! <seirosso1@gmail.com>
2021-09-27 22:55:35 +0300

    Lesson 3.1

---
+++ src/main/java/lesson3and1/Main.java
@@ -0,0 +1,28 @@
+package lesson3and1;
+
+public class Main {
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int threeCount = 3;
+        int fourCount = 4;
+        int fiveCount = 5;
+        String str = "Hello";
+        char d = 'd';
+        char e = 'e';
+        Integer res = 10;
+        double point = 1.2;
+        float war = 2.7f;
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(threeCount);
+        System.out.println(fourCount);
+        System.out.println(fiveCount);
+        System.out.println(str);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(res);
+        System.out.println(point);
+        System.out.println(war);
+    }
+}
