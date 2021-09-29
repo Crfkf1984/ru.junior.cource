63fbc3b6434e623d0dfffbe3614e5e0b43a3a9c9
Crfkf1984! <seirosso1@gmail.com>
2021-09-29 20:59:17 +0300

    Lesson 6.1

---
+++ src/main/java/lesson6and1/Main.java
@@ -0,0 +1,16 @@
+package lesson6and1;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numb = new int[5];
+        numb[0] = 1;
+        numb[1] = 2;
+        numb[2] = 3;
+        numb[3] = 4;
+        numb[4] = 5;
+
+        for (int i = 0; i <= numb.length -1 ; i++) {
+            System.out.println(numb[i]);
+        }
+    }
+}
