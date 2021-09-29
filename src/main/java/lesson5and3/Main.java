aa7a27beaf817d56bd911352552020a9ba016194
Crfkf1984! <seirosso1@gmail.com>
2021-09-29 19:58:26 +0300

    Lesson 5.3

--- src/main/java/lesson5and3/Main.java
+++ src/main/java/lesson5and3/Main.java
@@ -8,13 +8,49 @@ public class Main {
 
         System.out.println();
 
-        for (int i = 220; i <= 250; i++) {
+        int i = 200;
+        while (i <= 210) {
             System.out.println(i);
+            i++;
         }
 
-        for (int i = 200; i <= 250; i++) {
-            if (i <= 210 || i >= 220)
-            System.out.println(i);
+        System.out.println();
+
+        for (int w = 220; w <= 250; w++) {
+            System.out.println(w);
+        }
+
+        System.out.println();
+
+        int j = 220;
+        while (j <= 250) {
+            System.out.println(j);
+            j++;
+        }
+
+        System.out.println();
+
+        for (int s = 200; s <= 250; s++) {
+            System.out.println(s);
+            if (s == 210) {
+                s = 220;
+                System.out.println(s);
+
+            }
+
+        }
+
+        System.out.println();
+
+        int a = 200;
+        while (a <= 250) {
+            System.out.println(a);
+            a++;
+            if (a == 210) {
+                System.out.println(a);
+                a = 220;
+            }
+
         }
     }
 }
