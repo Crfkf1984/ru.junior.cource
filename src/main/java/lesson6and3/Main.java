f6960b6a2cd5f64dfcd9bb3cd8b066938bc1dd0b
Crfkf1984! <seirosso1@gmail.com>
2021-09-30 10:18:53 +0300

    Lesson 6.3

--- src/main/java/lesson6and3/Main.java
+++ src/main/java/lesson6and3/Main.java
@@ -1,18 +1,28 @@
 package lesson6and3;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] numb = new int[5];
-        numb[0] = 1;
-        numb[1] = 2;
-        numb[2] = 3;
-        numb[3] = 4;
-        numb[4] = 5;
+        numb[0] = 11;
+        numb[1] = 12;
+        numb[2] = 13;
+        numb[3] = 14;
+        numb[4] = 15;
 
         int s = 0;
-        for (int i = 0; i < numb.length ; i++) {
-          numb[i] = numb.length - i;
-            System.out.println(numb[i]);
+        int r = 0;
+        for (int i = 0; i < numb.length; i++) {
+             s =  numb[i];
+             numb[i] = numb[numb.length - i - 1];
+            numb[numb.length - i - 1] = s;
+            //System.out.println(numb[i]);
+            if(i == 2)
+                break;
         }
+
+        Arrays.stream(numb).forEach(System.out::println);
+
     }
 }
