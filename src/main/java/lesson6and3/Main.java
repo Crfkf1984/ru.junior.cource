0e4c183ef977e3fd80e78b84294ec55d452cacec
Crfkf1984! <seirosso1@gmail.com>
2021-09-30 12:11:28 +0300

    Lesson 6.3

--- src/main/java/lesson6and3/Main.java
+++ src/main/java/lesson6and3/Main.java
@@ -4,12 +4,17 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numb = new int[5];
+        int[] numb = new int[10];
         numb[0] = 11;
         numb[1] = 12;
         numb[2] = 13;
         numb[3] = 14;
         numb[4] = 15;
+        numb[5] = 16;
+        numb[6] = 17;
+        numb[7] = 18;
+        numb[8] = 19;
+        numb[9] = 20;
 
         int s = 0;
         int r = 0;
@@ -17,8 +22,7 @@ public class Main {
              s =  numb[i];
              numb[i] = numb[numb.length - i - 1];
             numb[numb.length - i - 1] = s;
-            //System.out.println(numb[i]);
-            if(i == 2)
+            if(i == numb.length / 2)
                 break;
         }
 
