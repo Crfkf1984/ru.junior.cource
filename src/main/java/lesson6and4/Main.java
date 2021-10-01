da5dc161b2ae5f5cbabb7fc2f7cdef29b259744c
Crfkf1984! <seirosso1@gmail.com>
2021-10-01 22:19:42 +0300

    Lesson 6.4

--- src/main/java/lesson6and4/Main.java
+++ src/main/java/lesson6and4/Main.java
@@ -4,25 +4,28 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int pazient = 10;
+        int[] pazient = new int[10];
         double min = 32;
         double max = 40;
         double res = Math.random() * ((max - min) + 1) + min;
         double temperatura = 0;
         int count = 0;
-        for (int i = 0; i < pazient; i++) {
+        int tempsum = 0;
+        double cons = 1e-7;
+        for (int i = 0; i < pazient.length; i++) {
             double numb = Math.round(Math.random() * ((max - min) + 1) + min);
             temperatura = (double) Math.round(res * 100) / 100;
 
-            if ((Math.abs(temperatura - 36.2) >= 1e-7) && (Math.abs(temperatura - 36.9)  >= 1e-7)) {
+            if ((Math.abs(temperatura - 36.2) >= cons) && (Math.abs(temperatura - 36.9)  >= cons)) {
                 count++;
-                temperatura += temperatura;
             }
+
+             tempsum += temperatura;
 
             System.out.print((double) Math.round(res * 100) / 100 + " ");
         }
         System.out.println();
-        double sredneeZnachenieTemperatur = temperatura / pazient;
+        double sredneeZnachenieTemperatur = tempsum / pazient.length;
         System.out.println("средняя температура " + (double) Math.round(sredneeZnachenieTemperatur * 100) / 100);
         System.out.println("Количество здоровых пациентов " + count);
     }
