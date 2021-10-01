b909a6e357ded55b94f0c33fde38e638430aa51a
Crfkf1984! <seirosso1@gmail.com>
2021-10-01 21:23:09 +0300

    Lesson 7.1

---
+++ src/main/java/lesson7and1/Main.java
@@ -0,0 +1,15 @@
+package lesson7and1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число !");
+        int res = scanner.nextInt();
+        for (int i = 1; i <= res; i++) {
+            int numb = res * i;
+            System.out.println(numb);
+        }
+    }
+}
