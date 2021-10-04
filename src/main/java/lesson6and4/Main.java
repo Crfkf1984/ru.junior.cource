f73da728e5779a32ae568a9fbd26f6bb7b1a3144
Crfkf1984! <seirosso1@gmail.com>
2021-10-04 20:33:54 +0300

    Lesson 6.4

--- src/main/java/lesson6and4/Main.java
+++ src/main/java/lesson6and4/Main.java
@@ -1,28 +1,26 @@
 package lesson6and4;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int[] pazient = new int[10];
+        double[] pazient = new double[10];
         double min = 32;
         double max = 40;
         int count = 0;
-        int tempsum = 0;
         double cons = 1e-0;
         for (int i = 0; i < pazient.length; i++) {
             double numbTemp = Math.round(Math.random() * ((max - min) + 1) + min);
-            double[] res = new double[10];
-            res[i] = numbTemp;
+            pazient[i] = numbTemp;
 
-            if (((Math.abs(res[i]) - 36.2) >= cons) && ((Math.abs(res[i]) - 36.9)  <= cons)) {
+            if (((Math.abs(pazient[i]) - 36.2) >= cons) && ((Math.abs(pazient[i]) - 36.9)  <= cons)) {
                 count++;
             }
 
-            tempsum += res[i];
-
-            System.out.print(res[i] + " ");
+            System.out.print(pazient[i] + " ");
         }
         System.out.println();
-        double sredneeZnachenieTemperatur = tempsum / pazient.length;
+        double sredneeZnachenieTemperatur = Arrays.stream(pazient).sum() / pazient.length;
         System.out.println("средняя температура " + (double) Math.round(sredneeZnachenieTemperatur * 100) / 100);
         System.out.println("Количество здоровых пациентов " + count);
     }
---
+++ src/main/java/lesson7and5/Main.java
@@ -0,0 +1,17 @@
+package lesson7and5;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numb = new int[10];
+        numb[0] = 1;
+        numb[1] = 8;
+        numb[2] = 3;
+        numb[3] = 3;
+        numb[4] = 5;
+        numb[5] = 5;
+        numb[6] = 5;
+        numb[7] = 8;
+        numb[8] = 8;
+        numb[9] = 8;
+    }
+}
