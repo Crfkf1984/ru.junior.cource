61bc964368774d1b9492f4e48d5da16899ce9c48
Crfkf1984! <seirosso1@gmail.com>
2021-10-08 20:08:48 +0300

    Lesson 7.5

--- src/main/java/lesson7and5/Main.java
+++ src/main/java/lesson7and5/Main.java
@@ -1,8 +1,13 @@
 package lesson7and5;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
-        int max = 0;
+        int count2 = 0;
+        int pupular1 = 0;
+        int popular2 = 0;
         int[] numb = new int[10];
         numb[0] = 1;
         numb[1] = 8;
@@ -11,22 +16,28 @@ public class Main {
         numb[4] = 5;
         numb[5] = 5;
         numb[6] = 5;
-        numb[7] = 8;
+        numb[7] = 5;
         numb[8] = 8;
         numb[9] = 8;
 
         for (int i = 0; i < numb.length; i++) {
-            int count = 0;
+            pupular1 = numb[i];
+            int count1 = 1;
             for (int j = i + 1; j < numb.length; j++) {
-                if (numb[i] == numb[j]) {
-                    numb[count]++;
+                if (pupular1 == numb[j]) {
+                    count1++;
                 }
+                if (count1 > count2) {
+                    popular2 = pupular1;
+                    count2 = count1;
+                } else if (count1 == count2) {
+                    popular2 = Math.max(popular2, pupular1);
+                }
+
             }
-            
-            if (numb[count] > max) {
-                max = numb[count];
-                System.out.println(numb[i]);
-            }
+
         }
+
+        System.out.println(popular2);
     }
 }
