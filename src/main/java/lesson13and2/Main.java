33fd5eb128006eb3de9e5c33d77414afb5820f23
Crfkf1984! <seirosso1@gmail.com>
2021-10-10 22:34:31 +0300

    Lesson 13.2

--- src/main/java/lesson13and2/Main.java
+++ src/main/java/lesson13and2/Main.java
@@ -1,18 +1,28 @@
 package lesson13and2;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Pattern pattern = Pattern.compile("[а-яА-Я]");
         System.out.println("Введите ФИО");
         String fio = scanner.nextLine();
-        String[] res = fio.split(" ");
-        if (res.length != 3) {
-            System.out.println("Введенная строка не является Ф.И.О");
-        } else {
+
+        if (fio.matches("[а-яА-Я,\\s]+")) {
+            String[] res = fio.split(" ");
+            if (res.length == 3) {
                 System.out.println("Фамилия : " + res[0] + "\n"
-                + "Имя : " + res[1] + "\n" + "Отчество : " + res[2]);
+                        + "Имя : " + res[1] + "\n" + "Отчество : " + res[2]);
+            } else {
+                System.out.println("Введенная строка не является Ф.И.О");
+            }
+        } else {
+            System.out.println("Вы ввели недопустимый символ в ФИО!");
         }
+
     }
 }
