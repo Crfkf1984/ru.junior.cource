3a33c8871ab41b86c9658efefc6cb7fd2eb2ce9e
Crfkf1984! <seirosso1@gmail.com>
2021-10-10 22:43:25 +0300

    Lesson 13.1

--- src/main/java/lesson13and1/Main.java
+++ src/main/java/lesson13and1/Main.java
@@ -6,8 +6,11 @@ public class Main {
 
         for (int i = 0; i < str.length(); i++) {
             char n = str.charAt(i);
+            char f = str.toLowerCase().charAt(i);
+            int rt = (int) f;
             int res = (int) n;
-            System.out.println("буква " + n + " ее код " + res);
+            System.out.println("буква " + n + " ее код " + res + "\n"
+                               + "буква " + f + " ее код " + rt);
         }
     }
 }
