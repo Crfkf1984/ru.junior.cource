5934ea30875d543f5441ad47e014515047cd26a7
Crfkf1984! <seirosso1@gmail.com>
2021-10-12 16:38:41 +0300

    Lesson 14.1

--- src/main/java/lesson14and1/Main.java
+++ src/main/java/lesson14and1/Main.java
@@ -2,21 +2,20 @@ package lesson14and1;
 
 public class Main {
     public static void main(String[] args) {
-        String str = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        String res = str.replaceAll("[., ]", " ");
-        String wer = res.replaceAll("[-А-я-]+", "");
-        String[] rt = wer.split(" ");
-        int count = 0;
-        for (String sd : rt) {
-            String conv = sd.trim();
-            if (!conv.equals("")) {
-                int num = Integer.parseInt(conv);
-                count += num;
-            } else {
-                continue;
+        String str = "Вася заработал -500 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+        str = str.replaceAll("[., ]", " ");
+        str = str.replaceAll("[А-я]+", "");
+            String[] rt = str.split(" ");
+            int count = 0;
+            for (String sd : rt) {
+                String conv = sd.trim();
+                if ((!conv.equals("")) && (!conv.equals("-"))) {
+                    int num = Integer.parseInt(conv);
+                    count += num;
+                } else {
+                    continue;
+                }
             }
+            System.out.println(count);
         }
-
-        System.out.println(count);
     }
-}
