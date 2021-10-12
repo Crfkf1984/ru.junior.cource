114b93bf9ad2f6000195b1b0f2c406dad15c694f
Crfkf1984! <seirosso1@gmail.com>
2021-10-12 17:42:34 +0300

    Lesson 14.2

--- src/main/java/lesson14and2/Main.java
+++ src/main/java/lesson14and2/Main.java
@@ -2,7 +2,7 @@ package lesson14and2;
 
 public class Main {
     public static void main(String[] args) {
-        String rt = "Before text editors existed, computer text was punched into cards with keypunch machines. " +
+        String rt = "Before  text editors existed, computer text was punched into cards with keypunch machines. " +
                 "Physical boxes of these thin cardboard cards were then inserted into a card-reader. Magnetic tape " +
                 "and disk card-image files created from such card decks often had no line-separation characters at all," +
                 " and assumed fixed-length 80-character records. An alternative to cards was punched paper tape. " +
@@ -15,15 +15,17 @@ public class Main {
                 " Some line editors could be used by keypunch; editing commands could be taken from a deck of cards and applied to a specified file." +
                 " Some common line editors supported a verify mode in which change commands displayed the altered lines.";
 
-        String[] res = word(rt);
+        String res = word(rt);
 
-        for (String wer : res) {
-            System.out.println(wer);
-        }
+            System.out.println(res);
     }
 
-    public static String[] word(String str){
-        String[] def = str.split(" ");
+    public static String word(String str){
+        String def = str.replace(" ", "\n");
+//        if (def.equals(str.matches("[0-9]"))) {
+//
+//        }
+//        }
         return def;
     }
 }
