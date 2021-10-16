b9d453e450ad0bee9c2fa1b0c53e4c3ada090962
Crfkf1984! <seirosso1@gmail.com>
2021-10-16 21:24:45 +0300

    Lesson 15.3

--- src/main/java/lesson15and3/Main.java
+++ src/main/java/lesson15and3/Main.java
@@ -2,22 +2,24 @@ package lesson15and3;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        Calendar calendar = Calendar.getInstance();
+        collectBirthDays(1984, 9, 3);
     }
 
     public static void collectBirthDays(int year, int month, int day) {
+        Calendar calendar = new GregorianCalendar(year, Calendar.SEPTEMBER, day);
+        Calendar now = Calendar.getInstance();
         SimpleDateFormat pattern = new SimpleDateFormat("dd.MM.yyyy - EE", Locale.US);
-        int numb = 0;
-        int today = new Date().getYear() + 1900;
 
-        for (int i = year; i < today; i++) {
-
-            System.out.println("" + numb + " - " + pattern.format(new Date(i - 1900, month - 1, day)));
+        int count = 0;
+        while (!calendar.after(now)) {
+            int numb = 1;
+            System.out.println("" + count++ + " - " + pattern.format(calendar.getTime()));
+            calendar.add(Calendar.YEAR, numb++);
         }
 
     }
