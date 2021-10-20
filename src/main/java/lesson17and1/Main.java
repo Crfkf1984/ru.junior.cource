5cf4e440082f3f5cd1a26bf18de37f612ea9e620
Crfkf1984! <seirosso1@gmail.com>
2021-10-20 22:31:41 +0300

    Lesson 17.1

--- src/main/java/lesson17and1/Main.java
+++ src/main/java/lesson17and1/Main.java
@@ -5,8 +5,15 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    public static List<String> list = new ArrayList<>();
+
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
+        final String ADD = "ADD";
+        final String EDIT = "EDIT";
+        final String DELETE = "DELETE";
+        final String EXIT = "EXIT";
+        final String LIST = "LIST";
+        //List<String> list = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         boolean res = true;
 
@@ -14,44 +21,77 @@ public class Main {
             System.out.println("Введите команду!");
             String word = scanner.nextLine();
 
-            if (word.startsWith("ADD")) {
-                list.add(word.substring(word.indexOf(" ")).trim());
-                System.out.println("Добавлено дело " + word.substring(word.indexOf(" ")));
+            if (word.startsWith(ADD)) {
+                add(word);
 
-            } else if (word.startsWith("EDIT")) {
-                int num = Integer.parseInt(word.replaceAll("[A-z]+", "").trim());
-                String str = word.replaceAll("\\d+", "").trim();
-                System.out.println("Дело " + list.get(num) + " заменено на " + str.substring(str.indexOf(" ")).trim());
-                list.remove(num);
-                list.add(num, str.substring(str.indexOf(" ")).trim());
+            } else if (word.startsWith(EDIT)) {
+                edit(word);
 
+            } else if (word.startsWith(LIST)) {
+                List<String> rs = list(word);
 
-            } else if (word.startsWith("LIST")) {
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.println(i + " - " + list.get(i));
+                for (int i = 0; i < rs.size(); i++) {
+                    System.out.println(i + " - " + rs.get(i));
                 }
 
-            } else if (word.startsWith("DELETE")) {
-                boolean rt = false;
-                int anInt = Integer.parseInt(word.replaceAll("[A-z]+", "").trim());
-                for (int i = 0; i < list.size(); i++) {
-                    if (anInt == i) {
-                        System.out.println("Дело " + list.get(i) + " удалено!");
-                        list.remove(i);
-                        rt = true;
-                    }
+            } else if (word.startsWith(DELETE)) {
+                delete(word);
 
-                }
-
-                if (!rt) {
-                    System.out.println("Дело с таким номером не существует!");
-                }
-
-            } else if (word.startsWith("EXIT")) {
-                System.out.println("Вы вышли из программы!");
-               res = false;
+            } else if (word.startsWith(EXIT)) {
+                exit(word);
+                res = false;
             }
         }
 
+    }
+
+    public static void add(String word) {
+        list.add(word.substring(word.indexOf(" ")).trim());
+        String todo = word.substring(word.indexOf(" "));
+        System.out.println("Добавлено дело " + todo);
+    }
+
+    public static void edit(String word) {
+        String[] str = word.split(" ");
+        int num = Integer.parseInt(str[1]);
+
+        for (int i = 0; i < list.size(); i++) {
+            if (num == i) {
+                String result = "";
+                for (int j = 2; j < str.length; j++) {
+                    result += str[j] + " ";
+                }
+                System.out.println("Дело " + list.get(num) + " заменено на " + result);
+                list.remove(num);
+                list.add(num, result);
+            }
+        }
+    }
+
+    public static List<String> list(String word) {
+        List<String> str = new ArrayList<>();
+        if (list.size() == 0) {
+            System.out.println("В данном списке пока ничего нет!");
+        } else {
+            for (int i = 0; i < list.size(); i++) {
+                str.add(list.get(i));
+            }
+        }
+        return str;
+    }
+
+    public static void delete(String word) {
+        String[] str  = word.split(" ");
+        int anInt = Integer.parseInt(str[1]);
+        for (int i = 0; i < list.size(); i++) {
+            if (anInt == i) {
+                System.out.println("Дело " + list.get(i) + " удалено!");
+                list.remove(i);
+            }
+        }
+    }
+
+    public static void exit(String word) {
+        System.out.println("Вы вышли из программы!");
     }
 }
